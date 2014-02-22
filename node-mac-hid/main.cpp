01ee41b11ed40a56ffb03032b7c8f4641eba3308
dapuck <imccall@da-puck.com>
2014-02-22 10:42:29 -0600

    Initial Commit

---
+++ node-mac-hid/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  node-mac-hid
+//
+//  Created by Ian McCall on 2/22/14.
+//  Copyright (c) 2014 Ian McCall. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
