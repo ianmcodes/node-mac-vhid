3199de7b2fcbc8d0b0c073486eaec1402d86f59d
dapuck <imccall@da-puck.com>
2014-02-24 22:38:10 -0600

    Added mouse button control.

--- node-mac-hid/main.cpp
+++ node-mac-hid/main.cpp
@@ -3,11 +3,10 @@
 //  node-mac-hid
 //
 //  Created by Ian McCall on 2/22/14.
-//  Copyright (c) 2014 Ian McCall. All rights reserved.
+//  See LICENSE.txt
 //
 
 #include <CoreGraphics/CoreGraphics.h>
-//#include <AppKit/AppKit.h>
 #include <node.h>
 #include <v8.h>
 
@@ -18,9 +17,22 @@ using namespace v8;
  * Mouse getters and setters *
  *****************************/
 CGEventSourceRef evtSrc = CGEventSourceCreate(kCGEventSourceStateHIDSystemState);
+CGRect bounds = CGDisplayBounds(kCGDirectMainDisplay);
+double maxY = bounds.size.height;
+double maxX = bounds.size.width;
 
 void setMousePosition(double x, double y) {
 	// sanity check will go here
+	if (x < 0) {
+		x = 0;
+	} else if (x > maxX) {
+		x = maxX;
+	}
+	if (y < 0) {
+		y = 0;
+	} else if (y > maxY) {
+		y = maxY;
+	}
 	CGEventRef evt = CGEventCreateMouseEvent(evtSrc, kCGEventMouseMoved, CGPointMake(x, y), kCGMouseButtonLeft);
 	CGEventPost(kCGSessionEventTap, evt);
 	CFRelease(evt);
@@ -35,6 +47,27 @@ CGPoint getMousePosition() {
 
 void showMouse() {
 	CGDisplayShowCursor(kCGDirectMainDisplay);
+}
+
+void button(uint32_t btn, bool up) {
+	uint32_t evtType = 0;
+	if (btn == kCGMouseButtonLeft) {
+		if (up) {
+			evtType = kCGEventLeftMouseUp;
+		} else {
+			evtType = kCGEventLeftMouseDown;
+		}
+	} else if (btn == kCGMouseButtonRight) {
+		if (up) {
+			evtType = kCGEventRightMouseUp;
+		} else {
+			evtType = kCGEventRightMouseDown;
+		}
+	}
+	CGPoint pt = getMousePosition();
+	CGEventRef evt = CGEventCreateMouseEvent(evtSrc, evtType, pt, btn);
+	CGEventPost(kCGHIDEventTap, evt);
+	CFRelease(evt);
 }
 ///////////////////////////////
 
@@ -112,6 +145,27 @@ Handle<Value> mouseShow(const Arguments& args) {
 	return scope.Close(Undefined());
 }
 
+Handle<Value> mouseBtnDown(const Arguments& args) {
+	HandleScope scope;
+	uint32_t btn = 0;
+	if (args.Length() > 0 && args[0]->IsNumber()) {
+		btn = args[0]->Int32Value();
+	}
+	button(btn, false);
+	return scope.Close(Undefined());
+}
+
+
+Handle<Value> mouseBtnUp(const Arguments& args) {
+	HandleScope scope;
+	uint32_t btn = 0;
+	if (args.Length() > 0 && args[0]->IsNumber()) {
+		btn = args[0]->Int32Value();
+	}
+	button(btn, true);
+	return scope.Close(Undefined());
+}
+
 void init(Handle<Object> exports) {
 	exports->Set(String::NewSymbol("mouseMoveDelta"),
 				 FunctionTemplate::New(mouseMoveDelta)->GetFunction());
@@ -121,6 +175,10 @@ void init(Handle<Object> exports) {
 				 FunctionTemplate::New(mouseGetCurrentPosition)->GetFunction());
 	exports->Set(String::NewSymbol("mouseShow"),
 				 FunctionTemplate::New(mouseShow)->GetFunction());
+	exports->Set(String::NewSymbol("mouseBtnDown"),
+				 FunctionTemplate::New(mouseBtnDown)->GetFunction());
+	exports->Set(String::NewSymbol("mouseBtnUp"),
+				 FunctionTemplate::New(mouseBtnUp)->GetFunction());
 }
 
 NODE_MODULE(machid, init)
